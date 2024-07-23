4e8840641f0b9c471b5cab9bc670c41e09bd8495
tiagoc0sta <tiagodpv@gmail.com>
2024-07-23 11:35:36 -0400

    f

---
+++ 27_Memorize_cs193p_swiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  27_Memorize_cs193p_swiftUI
+//
+//  Created by Tiago de Freitas Costa on 2024-07-23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ 27_Memorize_cs193p_swiftUI/_7_Memorize_cs193p_swiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  _7_Memorize_cs193p_swiftUIApp.swift
+//  27_Memorize_cs193p_swiftUI
+//
+//  Created by Tiago de Freitas Costa on 2024-07-23.
+//
+
+import SwiftUI
+
+@main
+struct _7_Memorize_cs193p_swiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
